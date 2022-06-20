b201e7dad78e054f02fe21d013fb836c9cfb7c63
Mykaela Saenz <mickycore@gmail.com>
2022-06-19 22:02:00 -0500

    centered documents

--- assets/coding.js
+++ assets/coding.js
@@ -12,23 +12,33 @@
 // Questions on Javascript Fundementals
 
 // 1.Which of the folowing is the correct syntax to redirect a url using JavaScript? A. window.location='http://www.newlocation.com'; <- correct b. document location='http://www.newlocation.com' c. browser.location='http://newlocation.com'; d. navigator.location='http://newlocation.com';
-// Created elements for the HTML page
+// 2. Event handlers are a type of: A. Functions <- correct b. interface c. classes d. none of the above
+// 3. ___ specifies the property fo an event: a. name b. type <- correct c. value d. all of the above
+// 4. How do you create a function in JavaScript? a. function + myFunction() b. function = myFunction() c. function myFunction() <- correct d. function:myFunction()
+// 5. How do you find the number with the highest value of x and y? a. celi(x,y) b. top(x,y) c. Math.ceil(x,y) d. Math.max(x,y) <-- correct
+
+// Created elements for the introduction HTML page
 var body = document.body;
 var h1El = document.createElement("h1");
 var infoEl = document.createElement("div");
 var paragraphEl = document.createElement("p");
 var buttonEl = document.createElement("button");
 
-
+// text content elements
 h1El.textContent = "Welcome to my quiz!";
-paragraphEl.textContent = "I hope you enjoy the quiz";
+paragraphEl.textContent = "Here is a very short quiz to test your JavaScript knowledge. Try to answer each question as quickly and CORRECTLY, as you can! Each incorrect answer will cause you to lose points. Try your best to get first place on the Highscore board.";
 buttonEl.textContent = "Start";
 
+
+// appending of elements
 body.appendChild(h1El);
 body.appendChild(infoEl);
 body.appendChild(paragraphEl);
 body.appendChild(buttonEl);
 
-h1El.setAttribute("style", "margin:auto; width: 50%; text-algin:center;");
 
+body.setAttribute("style", "margin:auto; width: 50%; padding: 50px;");
+h1El.setAttribute("style", "margin:auto; width: 50%; text-algin:center; font-size: 100px; padding: 150px;");
+paragraphEl.setAttribute("style", "font-size: 25px;");
+buttonEl.setAttribute("style", "margin:auto; border-radius:20px; font-size: 20px; background-color: white;");
 generateBtn.addEventListener("click",);
